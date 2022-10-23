fc83692a943b28ccb902c999c18b142c32ee5703
Emerson Viegas <emerson.viegas@gmail.com>
2022-10-23 17:42:12 -0300

    exer maior e menor elemento array

--- 01-fundamentos/1-4-introducao-javascript/dia-02-Js-arrays-e-loop-for/exer-lidando-com-arrays/parte2.js
+++ 01-fundamentos/1-4-introducao-javascript/dia-02-Js-arrays-e-loop-for/exer-lidando-com-arrays/parte2.js
@@ -10,6 +10,35 @@ for(let index = 0; index < numbers.length; index += 1){
   if(numbers[index] > maiorElemento){
     maiorElemento = numbers[index];
   }
-
-
 }
+
+console.log(maiorElemento);
+
+// exercicios valores impares de um array
+
+let numImpar = 0;
+
+for(let index = 0; index < numbers.length; index += 1) {
+
+  if(numbers[index] % 2 !== 0){
+    numImpar += 1;
+  }
+}
+
+if(numImpar === 0){
+  console.log('Nenhum valor impar encontrado');
+}else{
+  console.log(numImpar);
+}
+
+// exercicios menor valor do array
+
+let menorElemento = numbers[0];
+
+for(let index = 0; index < numbers.length; index += 1){
+  if(numbers[index] < menorElemento){
+    menorElemento = numbers[index];
+  }
+}
+
+console.log(menorElemento)
