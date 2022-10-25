ed7dd5555d088b8ea4468ec28d47d2814697d5a3
Emerson Viegas <emerson.viegas@gmail.com>
2022-10-25 13:12:09 -0300

    exrcicios operações aritimeticas com funções

---
+++ 01-fundamentos/1-4-introducao-javascript/dia-04-js-objetos-e-fucoes/exer-funcoes-opera-aritim.js
@@ -0,0 +1,32 @@
+const a = 3;
+const b = 2;
+
+function adicao(a, b){
+  return a + b;
+};
+
+function subtracao(a, b){
+  return a - b;
+};
+
+function multiplicacao(a, b){
+  return a * b;
+};
+
+function divisao(a, b){
+  return a / b;
+}
+
+function modulo(a, b){
+  return a % b;
+}
+
+console.log(adicao(a, b));
+
+console.log(subtracao(a, b));
+
+console.log(multiplicacao(a, b));
+
+console.log(divisao(a, b));
+
+console.log(modulo(a, b));
