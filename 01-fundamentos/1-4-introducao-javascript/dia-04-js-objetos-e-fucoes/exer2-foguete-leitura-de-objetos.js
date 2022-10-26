e925906e4eff77f49582d6dfe577716402c11be1
Emerson Viegas <emerson.viegas@gmail.com>
2022-10-25 22:47:02 -0300

    acesso as chaves e impressão no console acessando a string e os valor dela

---
+++ 01-fundamentos/1-4-introducao-javascript/dia-04-js-objetos-e-fucoes/exer2-foguete-leitura-de-objetos.js
@@ -0,0 +1,24 @@
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+
+// console.log(leitor.livrosFavoritos[titulo]);
+
+// let infoDoLivro = 'livrosFavoritos';
+
+
+
+console.log('O livro favorito de ' + leitor.nome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
+
+// console.log(leitor.livrosFavoritos[0].titulo);
+
