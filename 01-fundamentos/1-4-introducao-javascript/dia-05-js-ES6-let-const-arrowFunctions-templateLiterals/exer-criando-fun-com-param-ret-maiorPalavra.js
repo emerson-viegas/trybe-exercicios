d559b817e265d851b1c7c1e2de3c99bd956a8111
Emerson Viegas <emerson.viegas@gmail.com>
2022-10-27 11:16:05 -0300

    Resolução do exercício que recebe frase como parametro

--- 01-fundamentos/1-4-introducao-javascript/dia-05-js-ES6-let-const-arrowFunctions-templateLiterals/exer-criando-fun-com-param-ret-maiorPalavra.js
+++ 01-fundamentos/1-4-introducao-javascript/dia-05-js-ES6-let-const-arrowFunctions-templateLiterals/exer-criando-fun-com-param-ret-maiorPalavra.js
@@ -3,4 +3,26 @@
 
 
 
-longestWord('Antônio foi no banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
+//longestWord('Antônio foi no banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
+
+
+
+const maiorPalavra = (text) => {
+
+  const buscadorPalavra = text.split(' ');
+  let palavraMaior = 0;
+  let palavraBuscada = '';
+
+  for(const resultado of buscadorPalavra){
+    if(resultado.length > palavraMaior){
+      palavraMaior = resultado.length;
+      palavraBuscada = resultado;
+    }
+
+  }
+    
+  
+  return palavraBuscada;
+}
+
+console.log(maiorPalavra('Antônio foi no banheiro e não sabemos o que aconteceu'));
