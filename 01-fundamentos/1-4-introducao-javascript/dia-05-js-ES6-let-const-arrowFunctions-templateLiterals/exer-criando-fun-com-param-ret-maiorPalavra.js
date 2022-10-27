7c199bb1a34be647f9946c7b5aad893c1fbfd0b4
Emerson Viegas <emerson.viegas@gmail.com>
2022-10-26 22:57:45 -0300

    primeira parte do exercicio de retornar a maior palavra da frase em parametro

---
+++ 01-fundamentos/1-4-introducao-javascript/dia-05-js-ES6-let-const-arrowFunctions-templateLiterals/exer-criando-fun-com-param-ret-maiorPalavra.js
@@ -0,0 +1,6 @@
+
+//🚀 Crie uma função que receba uma frase como parâmetro e retorne a maior palavra da frase.
+
+
+
+longestWord('Antônio foi no banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
